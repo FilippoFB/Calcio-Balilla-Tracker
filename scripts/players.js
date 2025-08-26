75159b57c933b8667a8babcdc4e449133f127f43
FilippoFB <81244929+FilippoFB@users.noreply.github.com>
2025-08-26 15:18:30 +0200

    Add files via upload

---
+++ scripts/players.js
@@ -0,0 +1,10 @@
+// Lista centrale dei giocatori
+window.DEFAULT_PLAYERS = [
+  "Bonaccorso Hadrien",
+  "Ceresoli Luca",
+  "D'Angelo Angelo",
+  "Di Donato Marco",
+  "Fusar Bassini Filippo",
+  "Rossi Andrea",
+  "Zagheni Marco"
+];
